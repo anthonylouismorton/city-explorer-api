b6621170729487d81b05fb51c86148bb5d8823d6
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-09-26 10:54:36 -0700

    Finished lab 10

--- movie.js
+++ movie.js
@@ -1,18 +1,29 @@
 const axios = require('axios')
+const cache = require('./cache')
 
 function createPoster(movieData){
   const movieDetails = movieData.data.results.map(movie => {
     return new Movie(movie);
   })
     return movieDetails;
-};
+}; 
 
 async function getMovies(request, response) {
   const query = request.query.searchQuery;
   const movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_DB_API_KEY}&query=${query}&page1`
-  const movieResponse = await axios.get(movieURL)
 
+  if(Date.now() - cache.movieCache[0] < 40000){
+    console.log('cache works')
+    response.status(200).send(cache.movieCache[1])
+    return;
+  }
+  else{
+  console.log('did not hit cache')
+  const movieResponse = await axios.get(movieURL)
+  cache.movieCache[0] = Date.now();
+  cache.movieCache[1] = createPoster(movieResponse);
   response.status(200).send(createPoster(movieResponse))
+  }
 }
 
 class Movie{
@@ -21,10 +32,9 @@ class Movie{
     this.overview = movie.overview;
     this.average_votes = movie.vote_average;
     this.total_votes = movie.vote_count;
-    this.image_url = movie.poster_path;
+    this.image_url = "https://image.tmdb.org/t/p/w500"+movie.poster_path;
     this.popularity = movie.popularity;
-    this.released_on = movie.released_on;
+    this.released_on = movie.release_date;
   }
 }
-
-module.exports = {getMovies}
+module.exports={getMovies}
